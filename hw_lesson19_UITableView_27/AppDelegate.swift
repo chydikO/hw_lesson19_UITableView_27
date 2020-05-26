dd5363b34b676b184fd7476016524ad2828bf793
Oleg Chudnovskiy <oleg.chudnovskiy@gmail.com>
2020-05-26 05:38:49 +0300

    empty project

---
+++ hw_lesson19_UITableView_27/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  hw_lesson19_UITableView_27
+//
+//  Created by Oleg Chudnovskij on 25.05.2020.
+//  Copyright © 2020 Oleg Chudnovskij. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
